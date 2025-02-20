af1457b436e8d97443cf425a581caa64194334eb
root <root@ip-172-31-5-212.ap-south-1.compute.internal>
2025-02-20 06:08:49 +0000

    dh

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, GitHub Actions!');
+  res.send('Hello,mu  GitHub Actions!');
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
